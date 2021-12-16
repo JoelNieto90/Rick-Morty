55b929cefb5fff57015aa59fd21f492f6adf9efa
Joel Nieto <joenieto90@hotmail.com>
2021-12-15 19:13:11 -0600

    Home Component add Card

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import Card from "./Card";
 import "./styles/Home.scss";
+import logo from "./assets/logo.png";
+import RickMorty from "./assets/rick&morty.png";
+import photo1 from "./assets/EpisodiosWall.jpg";
+import photo2 from "./assets/wallpapersPers.jpg";
 
 const Home = () => {
   return (
     <main className="Home">
-      <p>Api Rick & Morty</p>
+      <section className="Home_Img">
+        <img className="Home_Img--picture1" src={logo} alt="Logo" />
+        <img className="Home_Img--picture2" src={RickMorty} alt="RickMorty" />
+      </section>
+      <section className="Home_Cards">
+        <Card photo={photo1} text="Characters" link="characters" />
+        <Card photo={photo2} text="Episodes" link="espisodes" />
+      </section>
+      <section className="Home_Text">
+        <p>
+          Welcome to the <br />
+          Rick and Morty page.
+        </p>
+      </section>
     </main>
   );
 };
