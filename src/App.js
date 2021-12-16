54e8bd45b1e971ae18e5f487a1d4112d99edacfd
Joel Nieto <joenieto90@hotmail.com>
2021-12-15 18:00:43 -0600

    Navbar component and added to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles/NavBar.scss";
+import logo from "./assets/logorick.png"
+
+const NavBar = () => {
+  return (
+    <nav className="NavBar">
+      <div className="NavBar_Img">
+        <Link to="/">
+          <img className="NavBar_Img--picture" src={logo} alt="Logo" />
+        </Link>
+      </div>
+      <div className="NavBar_Link">
+        <Link to="/">HISTORY</Link>
+        <Link to="/">TRAILER</Link>
+        <Link to="/">EPISODES</Link>
+        <Link to="/">CHARACTERS</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
